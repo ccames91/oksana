77302a514e8c8aa3721966fa5eaf2fd79c3fb80a
chandler ames <eddiehawk1991@gmail.com>
2023-10-25 23:49:25 -0700

    iniatial install

---
+++ client/src/index.js
@@ -0,0 +1,19 @@
+project/
+├── client/
+│   ├── node_modules/
+│   ├── public/
+│   ├── src/
+│   ├── package.json
+│   └── ...
+├── server/
+│   ├── config/
+│   │   └── .env
+│   ├── controllers/
+│   ├── models/
+│   ├── routes/
+│   ├── node_modules/
+│   ├── package.json
+│   ├── db.js
+│   └── server.js
+└── package.json
+
